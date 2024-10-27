eb91f9365ae7efcbbf5797ed311c856ccf1df0de
test081420 <test081420@gmail.com>
2024-10-27 20:36:53 +0800

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,8 +1,8 @@
 // Simulate automatic download
 window.onload = function() {
     var link = document.createElement('a');
-    link.href = 'eicar.com';  // Path to the EICAR test file
-    link.download = 'eicar.com';
+    link.href = 'eicar.txt';  // Path to the EICAR test file
+    link.download = 'eicar.txt';
     link.style.display = 'none';
     document.body.appendChild(link);
     link.click();
